ef07b14eb3a40af0724fd0a30eaff5c4a8842bec
vishu1994 <vishalpandey801@gmail.com>
2019-04-01 22:38:57 +0530

    square commit

--- mathop.py
+++ mathop.py
@@ -4,6 +4,9 @@ def add(a,b):
 def multiply(a,b):
 	return a*b
 
+def square(a):
+	return a*a
+
 
 print("Addition:")
 print(add(2,3))
@@ -11,3 +14,6 @@ print(add(2,3))
 
 print("Multiplication is:")
 print(multiply(5,3))
+
+print("Square of a number is :")
+print(square(4))
